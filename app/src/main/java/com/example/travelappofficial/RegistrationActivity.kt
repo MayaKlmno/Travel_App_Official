58996695d0d527931465ddf34ce23a190ae12184
Maya Oniciuc <maya.klmno@gmail.com>
2025-05-20 10:03:29 -0700

    working on the registration activity

--- app/src/main/java/com/example/travelappofficial/RegistrationActivity.kt
+++ app/src/main/java/com/example/travelappofficial/RegistrationActivity.kt
@@ -1,4 +1,27 @@
 package com.example.travelappofficial
 
-class RegistrationActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelappofficial.databinding.ActivityRegistrationBinding
+
+
+class RegistrationActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityRegistrationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME) ?: ""
+        val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD) ?: ""
+
+        binding.editTextRegistraionUsername.setText(username)
+        binding.editTextPassword.setText(password)
+
+        binding.buttonRegistrationRegister.setOnClickListener {
+
+        }
+    }
 }
