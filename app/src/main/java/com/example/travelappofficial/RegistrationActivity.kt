cefa8a7e00a5a62e41fcbf0d321d5031e1cd1574
Maya Oniciuc <maya.klmno@gmail.com>
2025-05-20 10:08:58 -0700

    I was working on the RegistrationActivity, there are still a few errors, and I have not like finished the file itsel yet...

--- app/src/main/java/com/example/travelappofficial/RegistrationActivity.kt
+++ app/src/main/java/com/example/travelappofficial/RegistrationActivity.kt
@@ -1,7 +1,11 @@
 package com.example.travelappofficial
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.backendless.Backendless
+import com.backendless.BackendlessUser
+import com.backendless.async.callback.AsyncCallback
 import com.example.travelappofficial.databinding.ActivityRegistrationBinding
 
 
@@ -13,7 +17,7 @@ class RegistrationActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME) ?: ""
         val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD) ?: ""
 
@@ -21,6 +25,29 @@ class RegistrationActivity : AppCompatActivity(){
         binding.editTextPassword.setText(password)
 
         binding.buttonRegistrationRegister.setOnClickListener {
+            val password = binding.editTextTextPassword.text.toString()
+            val confirm = binding.editTextRegistrationConformPassword.text.toString()
+            val username = binding.editTextRegistrationUsername.text.toString()
+            val name = binding.editTextRegistrationName.text.toString()
+            val email = binding.editTextRegistrationEmail.text.toString()
+
+            if(password == confirm){
+                val user = BackendlessUser()
+                user.setProperty("email", email)
+                user.setProperty("username", username)
+                user.password = password
+
+                Backendless.UserService.register(user, object: AsyncCallback<BackendlessUser>?) {
+                    override fun handleResponse(response: BackendlessUser?){
+                        val intent = Intent()
+                        intent.putExtra(LoginActivity.EXTRA_USERNAME, username)
+                        intent.putExtra(LoginActivity.EXTRA_PASSWORD, password)
+                        setResult(RESULT_OK, intent)
+                        finish()
+                        // after this the user has been registered and can now login
+                    }
+                }
+            }
 
         }
     }
