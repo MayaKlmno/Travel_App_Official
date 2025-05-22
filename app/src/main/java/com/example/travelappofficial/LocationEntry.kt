b5f8069b809f65ce0389ddb87f7c6adc3aa27ed6
audreye1 <audreyestanislao@gmail.com>
2025-05-22 09:41:29 -0700

    finished location entry data class

---
+++ app/src/main/java/com/example/travelappofficial/LocationEntry.kt
@@ -0,0 +1,27 @@
+package com.example.travelappofficial
+
+import java.util.Date
+import android.os.Parcelable
+import android.widget.RatingBar
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class LocationEntry(
+    var name: String = "Random Location",
+    var rating: Double = 0.0,
+    var moneySpent: Int = 0, // stored in cents (saves the decimal imprecision nonsense)
+    var emotion: String = EMOJI.MOUNTAIN.name,
+    var ownerId: String? = null,
+    var objectId: String? = null // null so that new objects receive an id from the server
+): Parcelable {
+    enum class EMOJI(var emoji: String) {
+        MOUNTAIN ("\uD83C\uDFD4"),
+        TROPICAL ("\uD83C\uDFDD"),
+        CITY("\uD83C\uDFD9"),
+        MYSTICAL("\uD83C\uDFEF"),
+        CASTLE("\uDBB9\uDCBF"),
+        CAMPING("\uD83C\uDFD5"),
+        DESERT("\uD83C\uDFDC")
+    }
+}
