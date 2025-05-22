3fb0c5925aa939aa81fd7587945305e044964454
audreye1 <audreyestanislao@gmail.com>
2025-05-22 09:09:14 -0700

    finished main activity

--- app/src/main/java/com/example/travelappofficial/MainActivity.kt
+++ app/src/main/java/com/example/travelappofficial/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.travelappofficial
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.travelappofficial.databinding.ActivityLoginBinding
+import com.example.travelappofficial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.buttonMainLogin.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+        binding.buttonMainSignup.setOnClickListener {
+//             this creates an intent object with the current activity and the destination activity's class
+            val registrationIntent = Intent(this, RegistrationActivity::class.java)
+            startActivity(registrationIntent)
+        }
+
     }
 }
