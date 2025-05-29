f4885c7c291873f5f8789e084874b0e1f94f8731
Maya Oniciuc <maya.klmno@gmail.com>
2025-05-29 08:51:24 -0700

    fixed an issue

--- app/src/main/java/com/example/travelappofficial/MainActivity.kt
+++ app/src/main/java/com/example/travelappofficial/MainActivity.kt
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
